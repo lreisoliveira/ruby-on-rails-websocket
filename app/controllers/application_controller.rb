2ea2add72f312fc7ca742e7f82955e83a4a17975
Leandro dos Reis Oliveira <lroliveira@fcl.com.br>
2016-06-29 15:47:28 -0300

    Criado route para testes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    return render json: {'numero': (1..99999).to_a.sample }, status: :ok
+  end
+
 end
